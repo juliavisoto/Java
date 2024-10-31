40075b263cadabff703181af07ec043719a08592
Julia Visoto <128156753+juliavisoto@users.noreply.github.com>
2024-10-31 08:16:40 -0300

    .

---
+++ main.java
@@ -0,0 +1,12 @@
+class Helloworld {
+  public static void main(String[] args) {
+    System.out.println("Esse e o Screen Match");
+    System.out.println("Filme: O menino do pijama listrado");
+    
+    int ano = 2008;
+    boolean incluidoNoPlano = true;
+    double notaFilme = 4.6
+    
+    System.out.println("Ano de lancamento " + ano);
+  }
+}
