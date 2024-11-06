185979de22ae74225bf97960a2f18a4a106b90ef
Julia Visoto <128156753+juliavisoto@users.noreply.github.com>
2024-11-06 08:58:54 -0300

    a

--- main.java
+++ main.java
@@ -8,5 +8,13 @@ class Helloworld {
     double notaFilme = 4.6
     
     System.out.println("Ano de lancamento " + ano);
+
+    double media = (8.4 + 7.5 + 6.8)/3;
+
+    System.out.println("A media das notas desse filme e: " + 4.0);
+
+    String sinopse;
+    sinopse = "um menino nazista que fez amizade com um judeu, e na inocencia trocou de lugar com ele no campo de concentraçao "
+    System.out.println(sinopse);
   }
 }
